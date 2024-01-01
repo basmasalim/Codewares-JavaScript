db81b558fa61f5baa7fadc0e580ffd567e15befb
Basma Hawash <125481348+basmasalim@users.noreply.github.com>
2024-01-01 13:31:41 +0200

    Delete 002-Sum Of Positives directory

--- 002-Sum Of Positives/theSecondMethod.js
+++
@@ -1,5 +0,0 @@
-
-function positiveSum(arr) {
-    return arr.filter((x) => x > 0).reduce((acc, current) => acc + current, 0);
-  }
-  console.log(positiveSum([-1, -2, 8]));
--- 002-Sum Of Positives/theThirdMethod.js
+++
@@ -1,8 +0,0 @@
-function positiveSum(arr) {
-    let sumPositive =0;
-    for (let i = 0; i < arr.length; i++) {
-      arr[i]>0? sumPositive+=arr[i]: 0 ;   
-    }
-    return sumPositive;
-  }
-  console.log(positiveSum([1,5,-1,7,-2]));
